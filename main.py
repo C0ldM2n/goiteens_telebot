41a8ded27ed9bcf83f3ce4f18d679a79d1e86d64
C0ldM2n <kostyadurlov@gmail.com>
2023-06-17 15:02:15 +0300

    Added Link button and menu

--- main.py
+++ main.py
@@ -6,30 +6,24 @@ bot = telebot.TeleBot('6052072835:AAGW8CGANoe2lexM0mm_R5kRCGvXel8xZiM')  # use t
 
 @bot.message_handler(commands=['start'])  # start message
 def start(message):
-    markup = types.ReplyKeyboardMarkup()
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     btn1 = types.KeyboardButton('Пройти опрос')
     btn2 = types.KeyboardButton(text='Перейти на сайт')
 
     markup.row(btn1, btn2)
     bot.send_message(message.chat.id, f'{message.from_user.first_name}, вітаю 👋. З чим можу допомогти?')
-    bot.send_sticker(message.chat.id, "CAACAgIAAxkBAAMLZIz0ROIeKV5cHYSaiDWUdIdpFOcAAjgLAAJO5JlLMrFH0tlPjNAvBA", reply_markup=markup)
-    #bot.register_next_step_handler(message, on_click)
+    bot.send_sticker(message.chat.id, 'CAACAgIAAxkBAAMLZIz0ROIeKV5cHYSaiDWUdIdpFOcAAjgLAAJO5JlLMrFH0tlPjNAvBA',
+                     reply_markup=markup)
+    bot.register_next_step_handler(message, on_click)
 
 
-@bot.message_handler(content_types=['text'])
-def bot_message(message):
-    if message == btn2:
-        bot.send_message = InlineKeyboardMarkup [InlineKeyboardButton(text='на гарний сайт', url='https://www.lifecell.ua/') ]
-
-
-#def on_click(message):
- #   if message.text == 'Перейти на сайт':
-  #      bot.
-
-#@bot.callback_query_handlers(func=lambda callback: True)
-#def callback_message(callback):
- #   if callback.data == 'delete':
-  #      bot.delete_message()
+def on_click(message):
+    if message.text == 'Перейти на сайт':
+        markup = types.InlineKeyboardMarkup()
+        linkbutton = types.InlineKeyboardButton('Перейти на сайт', url='https://www.lifecell.ua/uk/mobilnij-zvyazok'
+                                                                       '/taryfy/')
+        markup.add(linkbutton)
+        bot.send_message(message.chat.id, 'Нажмите ниже'.format(message.from_user), reply_markup=markup)
 
 
 bot.polling(none_stop=True)  # nonstop working
