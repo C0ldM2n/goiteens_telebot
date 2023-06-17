8a2837cd11b9aa7bab70f0f99f5f3f6637f2839d
C0ldM2n <kostyadurlov@gmail.com>
2023-06-17 11:36:46 +0300

    Put token in main.py

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
 import telebot
-import json
 
-with open('config.json', 'r', encoding='utf-8') as file:
-    config = json.load(file)
-
-bot = telebot.TeleBot(config['token'])
+bot = telebot.TeleBot('6052072835:AAGW8CGANoe2lexM0mm_R5kRCGvXel8xZiM')  # use token from config for token
 
 
-@bot.message_handler(commands=['start'])
+@bot.message_handler(commands=['start'])  # start message
 def main(message):
-    bot.send_message(message.chat.id, f'{message.from_user.first_name}, вітаю. З чим можу допомогти?')
+    bot.send_message(message.chat.id, f'{message.from_user.first_name}, вітаю u"\". З чим можу допомогти?')
     bot.send_sticker(message.chat.id, 'CAACAgIAAxkBAAMLZIz0ROIeKV5cHYSaiDWUdIdpFOcAAjgLAAJO5JlLMrFH0tlPjNAvBA')
 
 
-bot.polling(none_stop=True)
+bot.polling(none_stop=True) # nonstop working
