79dd37998aa97eed6e2e281c03ba381d7209dfe3
C0ldM2n <kostyadurlov@gmail.com>
2023-06-17 11:15:48 +0300

    Make base on Telebot lib

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import telebot
-from telebot import types
 import json
-import functions as fu
 
 with open('config.json', 'r', encoding='utf-8') as file:
     config = json.load(file)
@@ -10,28 +8,9 @@ bot = telebot.TeleBot(config['token'])
 
 
 @bot.message_handler(commands=['start'])
-def handle_start(message):
-    # Отправка приветственного сообщения
-    bot.reply_to(message, "Привет, я бот!")
+def main(message):
+    bot.send_message(message.chat.id, f'{message.from_user.first_name}, вітаю. З чим можу допомогти?')
+    bot.send_sticker(message.chat.id, 'CAACAgIAAxkBAAMLZIz0ROIeKV5cHYSaiDWUdIdpFOcAAjgLAAJO5JlLMrFH0tlPjNAvBA')
 
 
-@bot.message_handler(func=lambda message: True)
-def handle_message(message):
-    # Ваша логика обработки сообщения
-    response = process_message(message.text)
-
-    if response is None:
-        # Ответить, если бот не смог понять сообщение
-        bot.reply_to(message, "Извините, я не понимаю ваше сообщение.")
-    else:
-        # Ответить с обработанным сообщением
-        bot.reply_to(message, response)
-
-def process_message(text):
-    # Здесь вы можете добавить свою собственную логику обработки сообщений
-    # и возвратить обработанное сообщение или None, если бот не понял
-
-    return None
-
-
-bot.polling()
+bot.polling(none_stop=True)
