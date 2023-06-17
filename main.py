32da09f5b8205338f9d2f3683614e97fd505d65f
C0ldM2n <kostyadurlov@gmail.com>
2023-06-17 16:55:19 +0300

    Stable base v1

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 import telebot
 from telebot import types
 
-bot = telebot.TeleBot('6052072835:AAGW8CGANoe2lexM0mm_R5kRCGvXel8xZiM')  # use token from config for token
+bot = telebot.TeleBot('6052072835:AAGW8CGANoe2lexM0mm_R5kRCGvXel8xZiM')
 
 
 @bot.message_handler(commands=['start'])  # start message
 def start(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    btn1 = types.KeyboardButton('Пройти опрос')
-    btn2 = types.KeyboardButton(text='Перейти на сайт')
-
+    btn1 = types.KeyboardButton('💯 Пройти опитування')
+    btn2 = types.KeyboardButton('💻 Перейти на сайт')
     markup.row(btn1, btn2)
     bot.send_message(message.chat.id, f'{message.from_user.first_name}, вітаю 👋. З чим можу допомогти?')
     bot.send_sticker(message.chat.id, 'CAACAgIAAxkBAAMLZIz0ROIeKV5cHYSaiDWUdIdpFOcAAjgLAAJO5JlLMrFH0tlPjNAvBA',
@@ -17,13 +16,15 @@ def start(message):
     bot.register_next_step_handler(message, on_click)
 
 
+@bot.message_handler(content_types=['text'])  # LINK BUTTON
 def on_click(message):
-    if message.text == 'Перейти на сайт':
+    if message.text == '💻 Перейти на сайт' or 'Перейти на сайт' or 'Сайт' or 'сайт':
         markup = types.InlineKeyboardMarkup()
-        linkbutton = types.InlineKeyboardButton('Перейти на сайт', url='https://www.lifecell.ua/uk/mobilnij-zvyazok'
-                                                                       '/taryfy/')
+        linkbutton = types.InlineKeyboardButton('🌐 Перейти на сайт', url='https://www.lifecell.ua/uk/mobilnij-zvyazok'
+                                                                         '/taryfy/')
         markup.add(linkbutton)
-        bot.send_message(message.chat.id, 'Нажмите ниже'.format(message.from_user), reply_markup=markup)
+        bot.send_message(message.chat.id, 'Натисніть нижче ⬇'.format(message.from_user), reply_markup=markup)
+    # survey
 
 
 bot.polling(none_stop=True)  # nonstop working
