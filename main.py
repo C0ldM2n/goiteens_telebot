c27b0420f9397ee5679a3cbea4697770bc13e2a8
C0ldM2n <kostyadurlov@gmail.com>
2023-06-17 03:04:44 +0300

    First backup base

--- main.py
+++ main.py
@@ -9,12 +9,18 @@ dp = Dispatcher(bot=bot)
 
 @dp.message_handler(commands=['start'])
 async def cmd_start(message: types.Message):
-    await message.answer(f'{message.from_user.first_name}, ')
+    await message.answer(f'{message.from_user.first_name}, вітаю. З чим можу допомогти?')
+    await message.answer_sticker('CAACAgIAAxkBAAMLZIz0ROIeKV5cHYSaiDWUdIdpFOcAAjgLAAJO5JlLMrFH0tlPjNAvBA')
+
+
+@dp.message_handler(content_types=['sticker'])
+async def check_sticker(message: types.Message):
+    await message.answer(f'Ось id цього стікера:) :\n{message.sticker.file_id}')
 
 
 @dp.message_handler()
 async def answer(message: types.Message):
-    await message.reply('Я вас не розумію..')
+    await message.reply('Вибачте, але я вас не зрозумів..')
 
 
 if __name__ == '__main__':
